d3458af8f059321467278ccd89fda71fd29e9c16
Yugeyong <69104743+YUgyeong-Choi@users.noreply.github.com>
2025-07-22 22:57:32 +0900

    imgui 클라이언트에도 추가

--- Client/Public/Client_Defines.h
+++ Client/Public/Client_Defines.h
@@ -9,6 +9,18 @@
 #include <process.h>
 #include "Client_Enum.h"
 
+#ifdef USE_IMGUI
+#pragma push_macro("new")
+#undef new
+#include "imgui.h"
+#include "imgui_internal.h"
+#pragma pop_macro("new")
+#include "imgui_impl_dx11.h"
+#include "imgui_impl_win32.h"
+#include "ImGuizmo.h"
+#include "ImGuiFileDialog.h"
+using namespace ImGui;
+#endif
 
 namespace Client
 {
