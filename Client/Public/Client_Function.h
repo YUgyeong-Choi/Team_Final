40150ecdd551b6f9731e50f5784c4300b22a060d
Yugeyong <69104743+YUgyeong-Choi@users.noreply.github.com>
2025-08-05 15:22:32 +0900

    가만히 -> 움직임일때 카메라 타탁 수정

--- Client/Public/Client_Function.h
+++ Client/Public/Client_Function.h
@@ -12,6 +12,7 @@ static _float2 Get_MousePos()
 	return vMousePos;
 }
 
+/* 나중에 플레이어 static으로 가면 수정할 예정 */
 CGameObject* Get_Player(_uint iLevelIndex)
 {
 	return CGameInstance::Get_Instance()->Get_LastObject(iLevelIndex, TEXT("Layer_Player"));
