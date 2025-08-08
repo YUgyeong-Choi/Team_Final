2a5ffac1a29b450f02783a9013cd510bc8bf6501
Yugeyong <69104743+YUgyeong-Choi@users.noreply.github.com>
2025-08-08 11:19:39 +0900

    오류 수정

--- Client/Bin/ShaderFiles/Shader_VtxTrail.hlsl
+++ Client/Bin/ShaderFiles/Shader_VtxTrail.hlsl
@@ -1,133 +1,133 @@
-#include "Effect_Shader_Defines.hlsli"
-
-struct VS_IN
-{
-    float3 vPosition : POSITION;
-    float2 vPSize : PSIZE;
-    float2 vLifeTime : TEXCOORD0;
-};
-
-struct VS_OUT
-{
-    float3 vPosition : POSITION;
-    float2 vPSize : PSIZE;
-    float2 vLifeTime : TEXCOORD0;
-};
-
-VS_OUT VS_MAIN(VS_IN In)
-{
-    VS_OUT Out;
-    
-    // 이미 cpu쪽에서 다 줘가지고 연산 할 거리가 없네요??
-    Out.vPosition = In.vPosition;
-    Out.vPSize = In.vPSize;
-    Out.vLifeTime = In.vLifeTime;
-    
-    return Out;
-}
-
-struct GS_IN
-{
-    float3 vPosition : POSITION;
-    float2 vPSize : PSIZE;
-    float2 vLifeTime : TEXCOORD0;
-};
-
-struct GS_OUT
-{
-    float4 vPosition : SV_POSITION;
-    float2 vTexcoord : TEXCOORD0;
-    float2 vLifeTime : TEXCOORD1;
-    float4 vProjPos : TEXCOORD2;
-};
-
-[maxvertexcount(6)]
-void GS_MAIN(point GS_IN In[1], inout TriangleStream<GS_OUT> Triangles)
-{
-    GS_OUT Out[4];
-    
-    float3 vLook = g_vCamPosition.xyz - In[0].vPosition.xyz;
-    float3 vRight = normalize(cross(float3(0.f, 1.f, 0.f), vLook)) * In[0].vPSize.x * 0.5f;
-    float3 vUp = normalize(cross(vLook, vRight)) * In[0].vPSize.y * 0.5f;
-    
-    matrix matVP = mul(g_ViewMatrix, g_ProjMatrix);
-    
-    Out[0].vPosition = mul(float4(In[0].vPosition.xyz + vRight + vUp, 1.f), matVP);
-    Out[0].vTexcoord = float2(0.f, 0.f);
-    Out[0].vLifeTime = In[0].vLifeTime;
-    Out[0].vProjPos = float4(length(In[0].vPosition.xyz - g_vCamPosition.xyz), 0.f, 0.f, 0.f);
-    
-    Out[1].vPosition = mul(float4(In[0].vPosition.xyz - vRight + vUp, 1.f), matVP);
-    Out[1].vTexcoord = float2(1.f, 0.f);
-    Out[1].vLifeTime = In[0].vLifeTime;
-    Out[1].vProjPos = float4(length(In[0].vPosition.xyz - g_vCamPosition.xyz), 0.f, 0.f, 0.f);
-    
-    Out[2].vPosition = mul(float4(In[0].vPosition.xyz - vRight - vUp, 1.f), matVP);
-    Out[2].vTexcoord = float2(1.f, 1.f);
-    Out[2].vLifeTime = In[0].vLifeTime;
-    Out[2].vProjPos = float4(length(In[0].vPosition.xyz - g_vCamPosition.xyz), 0.f, 0.f, 0.f);
-    
-    Out[3].vPosition = mul(float4(In[0].vPosition.xyz + vRight - vUp, 1.f), matVP);
-    Out[3].vTexcoord = float2(0.f, 1.f);
-    Out[3].vLifeTime = In[0].vLifeTime;
-    Out[3].vProjPos = float4(length(In[0].vPosition.xyz - g_vCamPosition.xyz), 0.f, 0.f, 0.f);
-    
-    Triangles.Append(Out[0]);
-    Triangles.Append(Out[1]);
-    Triangles.Append(Out[2]);
-    //Triangles.RestartStrip(); // 이걸 안 써도 내가 밖에서 셰이더/버퍼쪽에서 만들 때 TriangleList임을 명시해서 괜찮다는 발언이 있는데 테스트해봄
-    
-    Triangles.Append(Out[0]);
-    Triangles.Append(Out[2]);
-    Triangles.Append(Out[3]);
-    //Triangles.RestartStrip();
-}
-
-
-struct PS_IN
-{
-    float4 vPosition : SV_POSITION;
-    float2 vTexcoord : TEXCOORD0;
-    float2 vLifeTime : TEXCOORD1;
-    float4 vProjPos : TEXCOORD2;
-};
-
-struct PS_OUT
-{
-    vector vColor : SV_TARGET0;
-};
-
-PS_OUT PS_MAIN(PS_IN In)
-{
-    PS_OUT Out;    
-    
-    Out.vColor = g_DiffuseTexture.Sample(DefaultSampler, In.vTexcoord);
-    
-    return Out;    
-}
-
-technique11 DefaultTechnique
-{
-    pass Default            // 0
-    {
-        SetRasterizerState(RS_Cull_None);
-        SetDepthStencilState(DSS_Default, 0);
-        SetBlendState(BS_AlphaBlend, float4(0.f, 0.f, 0.f, 0.f), 0xffffffff);
-        
-
-        VertexShader = compile vs_5_0 VS_MAIN();    
-        GeometryShader = NULL;
-        PixelShader = compile ps_5_0 PS_MAIN();      
-    }
-    pass SoftEffectOnly     // 1
-    {
-        SetRasterizerState(RS_Cull_None);
-        SetDepthStencilState(DSS_Default, 0);
-        SetBlendState(BS_AlphaBlend, float4(0.f, 0.f, 0.f, 0.f), 0xffffffff);        
-
-        VertexShader = compile vs_5_0 VS_MAIN_BLEND();
-        GeometryShader = NULL;
-        PixelShader = compile ps_5_0 PS_MAIN_SOFTEFFECT();
-    }
-
-}
+//#include "Effect_Shader_Defines.hlsli"
+//
+//struct VS_IN
+//{
+//    float3 vPosition : POSITION;
+//    float2 vPSize : PSIZE;
+//    float2 vLifeTime : TEXCOORD0;
+//};
+//
+//struct VS_OUT
+//{
+//    float3 vPosition : POSITION;
+//    float2 vPSize : PSIZE;
+//    float2 vLifeTime : TEXCOORD0;
+//};
+//
+//VS_OUT VS_MAIN(VS_IN In)
+//{
+//    VS_OUT Out;
+//    
+//    // 이미 cpu쪽에서 다 줘가지고 연산 할 거리가 없네요??
+//    Out.vPosition = In.vPosition;
+//    Out.vPSize = In.vPSize;
+//    Out.vLifeTime = In.vLifeTime;
+//    
+//    return Out;
+//}
+//
+//struct GS_IN
+//{
+//    float3 vPosition : POSITION;
+//    float2 vPSize : PSIZE;
+//    float2 vLifeTime : TEXCOORD0;
+//};
+//
+//struct GS_OUT
+//{
+//    float4 vPosition : SV_POSITION;
+//    float2 vTexcoord : TEXCOORD0;
+//    float2 vLifeTime : TEXCOORD1;
+//    float4 vProjPos : TEXCOORD2;
+//};
+//
+//[maxvertexcount(6)]
+//void GS_MAIN(point GS_IN In[1], inout TriangleStream<GS_OUT> Triangles)
+//{
+//    GS_OUT Out[4];
+//    
+//    float3 vLook = g_vCamPosition.xyz - In[0].vPosition.xyz;
+//    float3 vRight = normalize(cross(float3(0.f, 1.f, 0.f), vLook)) * In[0].vPSize.x * 0.5f;
+//    float3 vUp = normalize(cross(vLook, vRight)) * In[0].vPSize.y * 0.5f;
+//    
+//    matrix matVP = mul(g_ViewMatrix, g_ProjMatrix);
+//    
+//    Out[0].vPosition = mul(float4(In[0].vPosition.xyz + vRight + vUp, 1.f), matVP);
+//    Out[0].vTexcoord = float2(0.f, 0.f);
+//    Out[0].vLifeTime = In[0].vLifeTime;
+//    Out[0].vProjPos = float4(length(In[0].vPosition.xyz - g_vCamPosition.xyz), 0.f, 0.f, 0.f);
+//    
+//    Out[1].vPosition = mul(float4(In[0].vPosition.xyz - vRight + vUp, 1.f), matVP);
+//    Out[1].vTexcoord = float2(1.f, 0.f);
+//    Out[1].vLifeTime = In[0].vLifeTime;
+//    Out[1].vProjPos = float4(length(In[0].vPosition.xyz - g_vCamPosition.xyz), 0.f, 0.f, 0.f);
+//    
+//    Out[2].vPosition = mul(float4(In[0].vPosition.xyz - vRight - vUp, 1.f), matVP);
+//    Out[2].vTexcoord = float2(1.f, 1.f);
+//    Out[2].vLifeTime = In[0].vLifeTime;
+//    Out[2].vProjPos = float4(length(In[0].vPosition.xyz - g_vCamPosition.xyz), 0.f, 0.f, 0.f);
+//    
+//    Out[3].vPosition = mul(float4(In[0].vPosition.xyz + vRight - vUp, 1.f), matVP);
+//    Out[3].vTexcoord = float2(0.f, 1.f);
+//    Out[3].vLifeTime = In[0].vLifeTime;
+//    Out[3].vProjPos = float4(length(In[0].vPosition.xyz - g_vCamPosition.xyz), 0.f, 0.f, 0.f);
+//    
+//    Triangles.Append(Out[0]);
+//    Triangles.Append(Out[1]);
+//    Triangles.Append(Out[2]);
+//    //Triangles.RestartStrip(); // 이걸 안 써도 내가 밖에서 셰이더/버퍼쪽에서 만들 때 TriangleList임을 명시해서 괜찮다는 발언이 있는데 테스트해봄
+//    
+//    Triangles.Append(Out[0]);
+//    Triangles.Append(Out[2]);
+//    Triangles.Append(Out[3]);
+//    //Triangles.RestartStrip();
+//}
+//
+//
+//struct PS_IN
+//{
+//    float4 vPosition : SV_POSITION;
+//    float2 vTexcoord : TEXCOORD0;
+//    float2 vLifeTime : TEXCOORD1;
+//    float4 vProjPos : TEXCOORD2;
+//};
+//
+//struct PS_OUT
+//{
+//    vector vColor : SV_TARGET0;
+//};
+//
+//PS_OUT PS_MAIN(PS_IN In)
+//{
+//    PS_OUT Out;    
+//    
+//    Out.vColor = g_DiffuseTexture.Sample(DefaultSampler, In.vTexcoord);
+//    
+//    return Out;    
+//}
+//
+//technique11 DefaultTechnique
+//{
+//    pass Default            // 0
+//    {
+//        SetRasterizerState(RS_Cull_None);
+//        SetDepthStencilState(DSS_Default, 0);
+//        SetBlendState(BS_AlphaBlend, float4(0.f, 0.f, 0.f, 0.f), 0xffffffff);
+//        
+//
+//        VertexShader = compile vs_5_0 VS_MAIN();    
+//        GeometryShader = NULL;
+//        PixelShader = compile ps_5_0 PS_MAIN();      
+//    }
+//    pass SoftEffectOnly     // 1
+//    {
+//        SetRasterizerState(RS_Cull_None);
+//        SetDepthStencilState(DSS_Default, 0);
+//        SetBlendState(BS_AlphaBlend, float4(0.f, 0.f, 0.f, 0.f), 0xffffffff);        
+//
+//        VertexShader = compile vs_5_0 VS_MAIN_BLEND();
+//        GeometryShader = NULL;
+//        PixelShader = compile ps_5_0 PS_MAIN_SOFTEFFECT();
+//    }
+//
+//}
