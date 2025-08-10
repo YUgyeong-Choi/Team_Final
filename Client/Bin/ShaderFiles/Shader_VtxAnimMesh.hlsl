a6e67a94455d74f0112100fca99abe8d0886423c
dongha <leesukyi5@naver.com>
2025-08-10 19:50:48 +0900

    버그 수정

--- Client/Bin/ShaderFiles/Shader_VtxAnimMesh.hlsl
+++ Client/Bin/ShaderFiles/Shader_VtxAnimMesh.hlsl
@@ -369,7 +369,7 @@ technique11 DefaultTechnique
         SetDepthStencilState(DSS_Default, 0);
         SetBlendState(BS_Default, float4(0.f, 0.f, 0.f, 0.f), 0xffffffff);
         
-        VertexShader = compile vs_5_0 VS_MAIN();
+        VertexShader = compile vs_5_0 VS_MAIN_NONPICK();
         GeometryShader = NULL;
         PixelShader = compile ps_5_0 PS_MAIN_NONPICK();
     }
