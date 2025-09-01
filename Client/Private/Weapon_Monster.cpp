6e49c253cad3f53c65a68244a3923204ae1f4997
kyunglae <rudfodkdlel@gmail.com>
2025-09-01 21:30:52 +0900

    플레이어 체력 바 최소치 남아있도록

--- Client/Private/Weapon_Monster.cpp
+++ Client/Private/Weapon_Monster.cpp
@@ -25,7 +25,7 @@ HRESULT CWeapon_Monster::Initialize_Prototype()
 HRESULT CWeapon_Monster::Initialize(void* pArg)
 {
 	/* [ 데미지 설정 ] */
-	m_fDamage = 18.f;
+	m_fDamage = 20.f;
 
 	MONSTER_WEAPON_DESC* pDesc = static_cast<MONSTER_WEAPON_DESC*>(pArg);
 
