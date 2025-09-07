8f4fb1f715739c6abe4ff42016ef5010dc39d3df
hero <hanwoong2042@naver.com>
2025-09-07 15:04:40 +0900

    부서지는 메쉬 중력적용 하는 중

--- Client/Public/BreakableMesh.h
+++ Client/Public/BreakableMesh.h
@@ -11,13 +11,13 @@ NS_END
 
 NS_BEGIN(Client)
 
-class CBreakableMesh : public CDynamicMesh
+class CBreakableMesh : public CGameObject
 {
 public:
-	typedef struct tagBreakableMeshDesc : public CDynamicMesh::DYNAMICMESH_DESC
+	typedef struct tagBreakableMeshDesc : public CGameObject::GAMEOBJECT_DESC
 	{
 		//본 메쉬
-		//wstring ModelName = {};
+		wstring ModelName = {};
 
 		//파트 메쉬 갯수
 		_uint iPartModelCount = { 0 };
@@ -38,6 +38,41 @@ public:
 	virtual void Late_Update(_float fTimeDelta) override;
 	virtual HRESULT Render() override;
 
+public:
+	virtual void On_CollisionEnter(CGameObject* pOther, COLLIDERTYPE eColliderType, _vector HitPos, _vector HitNormal)
+	{
+		_int a = 0;
+	}
+	virtual void On_CollisionStay(CGameObject* pOther, COLLIDERTYPE eColliderType, _vector HitPos, _vector HitNormal)
+	{
+		_int a = 0;
+	}
+	virtual void On_CollisionExit(CGameObject* pOther, COLLIDERTYPE eColliderType, _vector HitPos, _vector HitNormal)
+	{
+		_int a = 0;
+	}
+
+	// Ray로 인항 충돌을 하면 On_Hit를 호출함 (HitPos & HitNormal을 가지고 올 수 있음)
+	virtual void On_Hit(CGameObject* pOther, COLLIDERTYPE eColliderType)
+	{
+ 		_int a = 0;
+	}
+
+	virtual void On_TriggerEnter(CGameObject* pOther, COLLIDERTYPE eColliderType)
+	{
+		_int a = 0;
+	}
+	virtual void On_TriggerStay(CGameObject* pOther, COLLIDERTYPE eColliderType)
+	{
+		_int a = 0;
+	}
+	virtual void On_TriggerExit(CGameObject* pOther, COLLIDERTYPE eColliderType)
+	{
+		_int a = 0;
+	}
+
+
+
 private:
 	HRESULT Render_Model();
 	HRESULT Render_PartModels();
@@ -48,14 +83,16 @@ private:
 
 private:    /* [ 컴포넌트 ] */
 	vector<CModel*> m_pPartModelComs = {};
+	vector<CPhysXDynamicActor*> m_pPartPhysXActorComs = {};
 
-	//CModel* m_pModelCom = { nullptr };
+	CModel* m_pModelCom = { nullptr };
 	CShader* m_pShaderCom = { nullptr };
 
 protected:
 	HRESULT Ready_Components(void* pArg);
 	HRESULT Bind_ShaderResources();
 	HRESULT Ready_Collider();
+	HRESULT Ready_PartColliders();
 
 public:
 	static CBreakableMesh* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
