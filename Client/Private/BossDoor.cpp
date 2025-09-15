6c240212558444afcf3e3d1703684023730ee4f8
Yugeyong <69104743+YUgyeong-Choi@users.noreply.github.com>
2025-09-15 17:06:11 +0900

    축제인도자 문열기 컷씬 살짝

--- Client/Private/BossDoor.cpp
+++ Client/Private/BossDoor.cpp
@@ -419,7 +419,6 @@ void CBossDoor::Move_Player(_float fTimeDelta)
 		{
 			m_bMoveStart = false;
 			m_bRotationStart = true;
-			m_pPlayer->Get_TransfomCom()->Set_State(STATE::POSITION, m_vTargetPos);
 		}
 	}
 
@@ -439,7 +438,6 @@ void CBossDoor::Move_Player(_float fTimeDelta)
 			break;
 		}
 
-		m_pPlayer->Get_TransfomCom()->Set_State(STATE::POSITION, m_vTargetPos);
 
 		if (m_pPlayer->RotateToDoor(fTimeDelta, vTargetRotation))
 		{
