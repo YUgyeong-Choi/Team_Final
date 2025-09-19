a40a74be82207bd9eeb44859407b5bd0a8c00008
hero <hanwoong2042@naver.com>
2025-09-19 15:52:49 +0900

    기름 데칼 소환 성공

--- Client/Private/Oil.cpp
+++ Client/Private/Oil.cpp
@@ -98,7 +98,7 @@ void COil::Priority_Update(_float fTimeDelta)
 			m_pPlayer = pPlayer;
 		}
 	
-		//Spawn_Decal(XMVectorSet(5.f, 0.5f, 5.f, 0));
+		Spawn_Decal(XMVectorSet(2.f, 0.5f, 2.f, 0));
 
 	}
 }
@@ -182,17 +182,26 @@ HRESULT COil::Spawn_Decal(_fvector vDecalScale)
 	DecalDesc.PrototypeTag[ENUM_CLASS(CStatic_Decal::TEXTURE_TYPE::BC)] = TEXT("Prototype_Component_Texture_FireEater_Oil_BC");
 
 	DecalDesc.bHasLifeTime = true;
-	DecalDesc.fLifeTime = 50.f;
+	DecalDesc.fLifeTime = 10.f;
 
+	// 플레이어의 월드 행렬
 	_matrix WorldMatrix = m_pTransformCom->Get_WorldMatrix();
 
+	// 위치만 추출
+	_vector vPosition = WorldMatrix.r[3]; // 행렬의 4번째 행(translation)
+
+	// 위치 행렬 생성
+	_matrix PosMatrix = XMMatrixTranslationFromVector(vPosition);
+
+	// 데칼 스케일 적용
 	_matrix ScaleMatrix = XMMatrixScaling(
 		XMVectorGetX(vDecalScale),
 		XMVectorGetY(vDecalScale),
 		XMVectorGetZ(vDecalScale));
 
-	// [스케일 * 기존 월드 행렬]
-	XMStoreFloat4x4(&DecalDesc.WorldMatrix, ScaleMatrix * WorldMatrix);
+	// 최종 월드 행렬 (스케일 + 위치)
+	XMStoreFloat4x4(&DecalDesc.WorldMatrix, ScaleMatrix * PosMatrix);
+
 
 	if (FAILED(m_pGameInstance->Add_GameObject(ENUM_CLASS(LEVEL::KRAT_CENTERAL_STATION), TEXT("Prototype_GameObject_Static_Decal"),
 		ENUM_CLASS(LEVEL::KRAT_CENTERAL_STATION), TEXT("Layer_Static_Decal"), &DecalDesc)))
